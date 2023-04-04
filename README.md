a5a61c736727bae66dda507fd190c90894c0999b
Maalxi <maalxi38@gmail.com>
2023-04-04 09:22:16 +0200

    Mise à jour le 04-04-2023 - update

---
+++ README.md
@@ -0,0 +1,10 @@
+# Exo algo
+
+```shell
+git clone git@github.com:Magle-corp/exo_algo.git
+cd exo_algo
+npm install
+
+npx parcel index.html # si parcel fonctionne à l'échelle de votre projet
+parcel index.html # si parcel fonctionne à l'échelle de votre machine
+```
---
+++ day_1/script.ts
@@ -0,0 +1,43 @@
+// Cet import permet d'exploiter le fichier JSON dans notre fichier Javascript / Typescript
+import data from './data.json'
+
+// Exercice #1
+// Affichez "data" dans la console pour voir à quoi ressemble les données que vous aller manipuler
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+
+// Exercice #2
+// À l'aide d'une boucle parcourez le tableau et faite le total d'habitants de ces 10 grandes villes
+
+// Exercice #2.1
+// Créer une fonction recevant le tableau des villes en paramètre, c'est la fonction qui se chargera de faire la boucle et le total d'habitant
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+// Exercice #3
+// À l'aide d'une boucle parcourez le tableau, pour chaque item du tableau afficher dans la console une phrase comme :
+// "La ville <insérer nom de la ville> est la <insérer rang de la ville> plus grande ville du monde, avec <insérer nombre habitant> habitants"
+
+// Exercice #3.1
+// Au lieu de faire le console.log() dans votre boucle, créer une fonction qui recevra une ville en paramètre et qui fera le console.log()
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+// Exercice #4
+// À l'aide d'une boucle parcourez le tableau de villes et construire un tableau avec les villes de plus de 20 millions d'habitants.
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+// Exercice #5
+// À l'aide d'une boucle parcourez le tableau et construire un tableau des villes, ordonné par leur nombre d'habitant
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+// Exercice #6
+// À l'aide d'une boucle parcourez votre tableau des villes ordonnées et créer en une liste affichable en dans le HTML (voir createElement())
